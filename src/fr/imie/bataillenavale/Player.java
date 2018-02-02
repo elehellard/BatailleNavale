2722e19b3dd23e22ffb01e1795fbed7399cf78f7
Kahsyaj <j.coley@laposte.net>
2018-02-02 11:43:26 +0100

    Getters/settersPlater

--- src/fr/imie/bataillenavale/Player.java
+++ src/fr/imie/bataillenavale/Player.java
@@ -6,4 +6,42 @@ public class Player {
 	protected String name;
 	protected ArrayList<Ship> ships;
 	protected Map field;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the ships
+	 */
+	public ArrayList<Ship> getShips() {
+		return ships;
+	}
+	/**
+	 * @param ships the ships to set
+	 */
+	public void setShips(ArrayList<Ship> ships) {
+		this.ships = ships;
+	}
+	/**
+	 * @return the field
+	 */
+	public Map getField() {
+		return field;
+	}
+	/**
+	 * @param field the field to set
+	 */
+	public void setField(Map field) {
+		this.field = field;
+	}
+	
+	
 }
