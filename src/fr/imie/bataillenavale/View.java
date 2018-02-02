c73420dad0a3c744449ff563274a519bafc5af31
E368 <arnaud.piquet@wanadoo.fr>
2018-02-02 10:31:08 +0100

    view

--- src/fr/imie/bataillenavale/View.java
+++ src/fr/imie/bataillenavale/View.java
@@ -1,5 +1,16 @@
 package fr.imie.bataillenavale;
 
+import java.util.Map.Entry;;
+
 public class View {
-	
+	public static void showMy(Player player1){
+		System.out.println(Map player1);
+		
+	}
+
+	public static void showhis(Player player2){
+		System.out.println(Map player2);
+		
+	}
 }
+
