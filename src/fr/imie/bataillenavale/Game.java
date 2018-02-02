c34dfb6f8eb696acf49291690bc204e559d05ab7
Kahsyaj <j.coley@laposte.net>
2018-02-02 10:27:57 +0100

    Création des attrubuts/getters/setters de game

--- src/fr/imie/bataillenavale/Game.java
+++ src/fr/imie/bataillenavale/Game.java
@@ -1,6 +1,29 @@
 package fr.imie.bataillenavale;
 
 public class Game {
-	protected int shipNumber[];
+	protected int shipNumber;
 	protected Player players[];
+	
+	public Game(int shpNb, Player plyrs[]) {
+		this.shipNumber = shpNb;
+		this.players = plyrs;
+	}
+
+	public int getShipNumber() {
+		return shipNumber;
+	}
+
+	public void setShipNumber(int shipNumber) {
+		this.shipNumber = shipNumber;
+	}
+
+	public Player[] getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(Player[] players) {
+		this.players = players;
+	}
+	
+	
 }
