9593fb41d830903623a105c6af3042d53fa03b97
nicniv <nicolas.nivez@outlook.fr>
2018-02-02 11:44:27 +0100

    map nico

--- src/fr/imie/bataillenavale/Map.java
+++ src/fr/imie/bataillenavale/Map.java
@@ -1,5 +1,13 @@
 package fr.imie.bataillenavale;
 
 public class Map {
-	protected int field[];
+	private Ship field[][];
+
+	public boolean isValid(int x, int y) {
+		if (this.field[x][y] == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
