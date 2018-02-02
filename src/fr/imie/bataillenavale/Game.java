60971ec2ac071962602fb7d71ed0a306f9e4c7c9
Kahsyaj <j.coley@laposte.net>
2018-02-02 11:19:46 +0100

    Ajout des classes Player (getters/setters)/ Game

--- src/fr/imie/bataillenavale/Game.java
+++ src/fr/imie/bataillenavale/Game.java
@@ -1,26 +1,36 @@
 package fr.imie.bataillenavale;
 
+import java.util.Scanner;
+
+/**
+ * @author ahsyaj
+ *
+ */
 public class Game {
 	protected int shipNumber;
 	protected Player players[];
-	
-	public Game(int shpNb, Player plyrs[]) {
-		this.shipNumber = shpNb;
-		this.players = plyrs;
-	}
-
+	public static Scanner sc = null;
+	/**
+	 * @return the shipNumber
+	 */
 	public int getShipNumber() {
 		return shipNumber;
 	}
-
+	/**
+	 * @param shipNumber the shipNumber to set
+	 */
 	public void setShipNumber(int shipNumber) {
 		this.shipNumber = shipNumber;
 	}
-
+	/**
+	 * @return the players
+	 */
 	public Player[] getPlayers() {
 		return players;
 	}
-
+	/**
+	 * @param players the players to set
+	 */
 	public void setPlayers(Player[] players) {
 		this.players = players;
 	}
--- src/fr/imie/bataillenavale/Map.java
+++ src/fr/imie/bataillenavale/Map.java
@@ -1,5 +1,15 @@
 package fr.imie.bataillenavale;
 
 public class Map {
-	protected int field[];
+	protected int field[][];
+	
+	public Map(int size) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				this.field[i][j] = null;
+			}
+		}
+	}
+	
+	
 }
